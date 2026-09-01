ed2656730c5240e4c8b51394950fdf6be750293e
Akshara Garg <aksharagarg666@gmail.com>
2026-09-01 12:33:56 +0530

    fix cross-site cookie settings

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -139,8 +139,8 @@ const loginUser = asyncHandler(async (req, res) => {
 
     const options = {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "lax",
+        secure: true,
+        sameSite: "none",
     };
 
     return res
